d1726c8c25443ec15735707ecb4f29532afc670b
Matthew Gharrity <gharrma@google.com>
2021-04-10 00:26:20 -0400

    tmp: Fix fastutil version

--- prepare/compiler/build.gradle.kts
+++ prepare/compiler/build.gradle.kts
@@ -209,7 +209,7 @@ dependencies {
     if (Platform.P202()) {
         fatJarContents(intellijDep()) { includeJars("intellij-deps-fastutil-8.3.1-1") }
     } else if (Platform.P203.orHigher()) {
-        fatJarContents(intellijDep()) { includeJars("intellij-deps-fastutil-8.3.1-3") }
+        fatJarContents(intellijDep()) { includeJars("intellij-deps-fastutil-8.4.1-4") }
     }
 
     fatJarContents(intellijDep()) { includeJars("lz4-java", rootProject = rootProject) }
